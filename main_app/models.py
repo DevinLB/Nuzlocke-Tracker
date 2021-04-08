98fb2cc556c45abcd4c27b97bee5c0ca476484f1
devinlb <hhodvp@hotmail.com>
2021-04-08 17:43:57 -0600

    Adds basic site layout

--- main_app/models.py
+++ main_app/models.py
@@ -6,10 +6,31 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Run(models.Model):
-    name = models.CharField(max_length=100)
-    game = models.CharField(max_length=100)
-    rules = models.CharField(max_length=1000)
+  name = models.CharField(max_length=100)
+  game = models.CharField(max_length=100)
+  rules = models.CharField(max_length=1000)
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.name
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name
+
+# class Pokemon(models.Model):
+#   name = models.CharField(max_length=100)
+#   pk_id = models.CharField(max_length=100)
+#   type_1 = models.CharField(max_length=15)
+#   type_2 = models.CharField(max_length=15, null=True)
+#   status = models.CharField(max_length=15, null=True)
+
+#   run = models.ForeignKey(Run, on_delete=models.CASCADE)
+#   def __str__(self):
+#     return self.name
+
+# class Pkinfo(models.Model):
+#   name = models.CharField(max_length=100)
+#   pk_id = models.CharField(max_length=100)
+#   type_1 = models.CharField(max_length=15)
+#   type_2 = models.CharField(max_length=15, null=True)
+  
+
+  def __str__(self):
+    return self.name
