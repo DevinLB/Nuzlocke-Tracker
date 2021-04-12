5292839edd15778b14bbaea906020d4e8fec1bdd
devinlb <hhodvp@hotmail.com>
2021-04-12 01:31:04 -0600

    Before attempting any api database shenanigans

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.postgres.fields import JSONField
+
 # Import the User
 from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Run(models.Model):
@@ -11,6 +14,14 @@ class Run(models.Model):
   rules = models.CharField(max_length=1000)
 
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name
+
+class Pkinfo(models.Model):
+  name = models.CharField(max_length=100)
+  pk_id = models.CharField(max_length=100)
+  jfield = JSONField()
+
   def __str__(self):
     return self.name
 
@@ -25,12 +36,22 @@ class Run(models.Model):
 #   def __str__(self):
 #     return self.name
 
+
 # class Pkinfo(models.Model):
 #   name = models.CharField(max_length=100)
 #   pk_id = models.CharField(max_length=100)
 #   type_1 = models.CharField(max_length=15)
 #   type_2 = models.CharField(max_length=15, null=True)
   
-
   # def __str__(self):
   #   return self.name
+
+
+# class Game(models.Model):
+#   name = models.CharField(max_length=100)
+#   generation = models.CharField(max_length=100)
+#   areas = models.CharField(max_length=1000)
+#   pokedex = models.CharField(max_length=1000)
+
+#   def __str__(self):
+#     return self.name
